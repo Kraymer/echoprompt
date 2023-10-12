766d5cd18469427ccb204b7baace1920a40ffa2e
Fabrice Laporte <kraymer@gmail.com>
2023-10-12 14:48:14 +0200

    feat: add class EchoPrompt

--- echoprompt/__init__.py
+++ echoprompt/__init__.py
@@ -4,3 +4,21 @@ made and provide them as default.
 """
 
 __version__ = "0.0.1-beta"
+
+from InquirerPy import inquirer
+from trapdoor import Trapdoor
+
+
+class EchoPrompt:
+    def __init__(self, store_name):
+        self.history = Trapdoor(store_name)
+
+    def prompt_choice(self, name, choices):
+        res = inquirer.select(
+            message=name.replace("_", " ").title(),
+            choices=choices,
+            default=self.history.get(name),
+        ).execute()
+        if res:
+            self.history.set(name, res)
+        return res
